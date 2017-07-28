f01e75c4605230ac02cb7d39622e4168c807a461
pawanks <pawan.sharma@yahoo.com>
2017-07-28 10:49:18 +0530

    Docker Test added DockerFile and sample flask app

--- ansibleAtGlance/bootstrap-mgmt.sh
+++ ansibleAtGlance/bootstrap-mgmt.sh
@@ -4,9 +4,16 @@ yum install epel-release -y
 yum install ansible -y 
 yum install libselinux-python -y
 cp -pvr /vagrant/ansible /etc/
+mkdir -pv /root/.ssh
+su -m vagrant -c "ssh-keygen -f /home/vagrant/.ssh/id_rsa -t rsa -N ''"
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+yum install libselinux-python -y
+setenforce 0
+sudo /etc/init.d/sshd reload
 chown -R vagrant.vagrant /etc/ansible
 chown -R vagrant.vagrant /home/vagrant
 #ansible-playbook /etc/ansible/addSSHKey.yml --user=vagrant --extra-vars "ansible_sudo_pass=#{secret['password']}"
+sleep 5
 ansible-playbook /etc/ansible/addSSHKey.yml --extra-vars @/etc/ansible/secret --vault-password-file=/etc/ansible/vault.txt -e "ansible_ssh_user=vagrant ansible_ssh_pass={{my_pass}}"
 #key=`cat /home/vagrant/.ssh/id_rsa.pub`
 #echo $access_token
